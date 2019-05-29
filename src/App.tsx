c47ecaf76cb5eb685957b75c1d2122ea41fc9dd2
frontvin <bvs0618@gmail.com>
2019-05-29 16:33:02 +0300

    try normalize data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { normalize, schema } from "normalizr";
 import axios from "axios";
+import { normalize, schema } from "normalizr";
 import { Table } from "semantic-ui-react";
 import "./App.css";
 import { TableHeaderRow } from "./components/TableHeaderRow";
@@ -8,135 +8,62 @@ import { Task } from "./components/Task";
 
 const App = () => {
   //Data
-  const initialState: ITasksState = {
+  const data: ITasksState = {
     ids: [],
     tasksById: {}
   };
 
+  interface INormalizedData<T> {
+    [ids: string]: T;
+  }
+
   interface ITasksState {
-    readonly ids: TaskId[];
-    readonly tasksById: { [id: number]: IOneTask };
+    ids: TaskId[];
+    tasksById: { [id: number]: IOneTask };
   }
 
   interface IOneTask {
-    id: number;
+    id: TaskId;
     taskName: string;
     taskStart: number;
     taskDuration: number;
     cellColor: string;
   }
 
-  type TaskId = number;
-
-  interface INormalizedData<T> {
-    [uuid: string]: T;
-  }
-
   interface INormalizedTasksResponse {
     result: number[];
-    entities: INormalizedData<IOneTask>;
+    entities: {
+      tasks: INormalizedData<IOneTask>;
+    };
   }
 
-  const UnNormalizedData = {
-    tasks: [
-      {
-        id: 1,
-        taskName: "Task 1",
-        taskStart: 3,
-        taskDuration: 3,
-        cellColor: "red"
-      },
-      {
-        id: 2,
-        taskName: "Task 2",
-        taskStart: 1,
-        taskDuration: 3,
-        cellColor: "blue"
-      },
-      {
-        id: 3,
-        taskName: "Task 3",
-        taskStart: 4,
-        taskDuration: 4,
-        cellColor: "green"
-      },
-      {
-        id: 4,
-        taskName: "Task 4",
-        taskStart: 3,
-        taskDuration: 8,
-        cellColor: "yellow"
-      },
-      {
-        id: 5,
-        taskName: "Task 5",
-        taskStart: 1,
-        taskDuration: 6,
-        cellColor: "purple"
-      },
-      {
-        id: 6,
-        taskName: "Task 6",
-        taskStart: 2,
-        taskDuration: 9,
-        cellColor: "orange"
-      },
-      {
-        id: 7,
-        taskName: "Task 7",
-        taskStart: 4,
-        taskDuration: 5,
-        cellColor: "grey"
-      },
-      {
-        id: 8,
-        taskName: "Task 8",
-        taskStart: 3,
-        taskDuration: 5,
-        cellColor: "blue"
-      },
-      {
-        id: 9,
-        taskName: "Task 9",
-        taskStart: 5,
-        taskDuration: 1,
-        cellColor: "cyan"
-      },
-      {
-        id: 10,
-        taskName: "Task 10",
-        taskStart: 4,
-        taskDuration: 3,
-        cellColor: "red"
-      }
-    ]
-  };
+  type TaskId = number;
 
 
-  // Setting state
+  const unNormalizedData = axios.get("http://localhost:3000/tasks")
 
   // define task schema
   const task = new schema.Entity("tasks");
   const mySchema = { tasks: [task] };
 
-  // Normalized array
-  const normTasks = normalize(UnNormalizedData, mySchema);
+  // // Normalized array
+  const normTasks = normalize(unNormalizedData, mySchema);
 
   console.log(normTasks);
 
-  const [tasks, setTasks] = useState(initialState);
+  // Setting state
+  const [tasks, setTasks] = useState(normTasks);
 
   const postData = (data: ITasksState) => {
-    return axios
-      .post(`http://localhost:3000/tasksById`, data)
-      // .then(response => console.log(response));
+    return axios.post(`http://localhost:3000/tasksById`, data)
+      .then((response) => console.log(response))
   };
 
   const changeTaskDuration = (
     taskID: number,
     taskStart: number,
     taskDuration: number
-  ): void => {
+  ) : void  => {
     const task = tasks.tasksById[taskID];
     const newTasks = { ...tasks };
     newTasks.tasksById[taskID] = {
@@ -145,12 +72,12 @@ const App = () => {
     };
     setTasks(newTasks);
 
-    postData(tasks);
+    postData(tasks)
   };
 
   useEffect(() => {
     axios
-      .get<ITasksState>("http://localhost:3000/tasksById")
+      .get<ITasksState>("http://localhost:3000/tasks")
       .then(result => setTasks(result.data));
   }, []);
 
